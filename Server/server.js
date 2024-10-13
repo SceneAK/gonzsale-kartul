2221a7efe510d3db53098666e864089f67973289
kueworkspace-admin <36.evan.gunardi@gonzaga.sch.id>
2024-10-13 21:20:43 +0700

    Backend Boilerplate
    Note: Install NodeJS & Express, set up dependencies to work on the project

---
+++ Server/server.js
@@ -0,0 +1,30 @@
+// Create express app
+const express = require('express'); 
+const app = express()
+
+// load .env file to process.env
+require('dotenv').config();
+const PORT = process.env.PORT;
+
+// request logging
+app.use((req, res, next) => {console.log(req.path, req.method); next();})
+
+// parses req.json and puts it into req.body
+app.use(express.json());
+
+
+// MOUNTS ROUTES
+const defaultRoute = require('./routes/defaultRoute')
+app.use('/', defaultRoute);
+
+
+
+// error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Error')
+  })
+
+// Start Listening
+app.listen(PORT, () => {console.log(`listening on ${PORT}`)}) 
+
