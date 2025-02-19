36f6ac9c88cbf333aa8f00f09151b9ab2f9480c3
kueworkspace-admin <36.evan.gunardi@gonzaga.sch.id>
2025-02-19 17:41:44 +0700

    moved .env

--- Server/initialize.js
+++ Server/initialize.js
@@ -1,5 +1,5 @@
+import '../envLoader.js';
 import upath from 'upath';
-import dotenv from 'dotenv';
 import { fileURLToPath } from 'url';
 
 // Convert URL to file path
@@ -9,9 +9,6 @@ const __dirname = upath.dirname(thisFileName);
 // static routes
 const PUBLIC_DIR = upath.join(__dirname, '/public/'); 
 const STATIC_ROUTE_NAME = 'source';
-
-// load .env file to process.env
-dotenv.config();
 
 const env = process.env;
 export {
