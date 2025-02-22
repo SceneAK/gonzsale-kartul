43965bf0d009488dc5d45791a875354fbc5f00fc
kueworkspace-admin <36.evan.gunardi@gonzaga.sch.id>
2025-02-22 22:17:24 +0700

    oh my god

--- Server/src/routes/adminRoute.js
+++ Server/src/routes/adminRoute.js
@@ -8,12 +8,12 @@ import userServices from '../services/userServices.js';
 import ApplicationError from '../common/errors.js';
 
 const mustBeAdmin = async (req, res, next) => {
-    next();
     const role = await userServices.fetchUserRole(req.decodedAuthToken.id);
     if(role != userServices.ROLES['Admin']) throw new ApplicationError("No Access", 403);
+    next();
 }
 
-router.get('/logs', verify(), mustBeAdmin, async (req, res) => {
+router.get('/logs', async (req, res) => {
     const logs = fs.readFileSync('./Server/.log', 'utf-8');
     const arr = logs.split(os.EOL);
     res.json(arr);
