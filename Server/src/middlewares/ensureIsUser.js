a5fc61a870cd1a611a235294fd3a02c52a1426cb
kueworkspace-admin <36.evan.gunardi@gonzaga.sch.id>
2025-03-19 21:43:07 +0700

    Fixed Minor Bugs

--- Server/src/middlewares/ensureIsUser.js
+++ Server/src/middlewares/ensureIsUser.js
@@ -1,8 +1,8 @@
 import { ApplicationError } from "../common/index.js";
 
-const ensureIsUser = (paramName) => async (req, res, next) => {
+const ensureIsUser = (paramName = null) => async (req, res, next) => {
     const userIdParam = req.params[paramName];
-    if(req.authJwt?.id || (userIdParam && req.authJwt?.id != userIdParam))
+    if(!req.authJwt?.id || (userIdParam && req.authJwt?.id != userIdParam))
     {
         throw new ApplicationError("Current user cannot access requested User", 403);
     }
